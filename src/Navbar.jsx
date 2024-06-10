ac38af62c1a138241c448f7d8d7ccf7253d2f278
Artin karimi <artin962354@proton.me>
2024-06-10 14:55:52 +0330

    📝 Update the navbar texts

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -11,14 +11,14 @@ function Navbar() {
             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
           </div>
           <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            <li><a>AN EMOJI GUIDE FOR YOU COMMIT MESSAGES LIKE GITMOJI</a></li>
+            <li><a href="https://github.com/Thecode764/Gitem">🐈 Github</a></li>
           </ul>
         </div>
         <a className="btn btn-ghost text-xl">GITEM</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
-          <li><a>AN EMOJI GUIDE FOR YOU COMMIT MESSAGES LIKE GITMOJI</a></li>
+          <li><a href="https://github.com/Thecode764/Gitem">🐈 Github</a></li>
         </ul>
       </div>
       <div className="navbar-end">
